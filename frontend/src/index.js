f70d7162b26902b9bb9bf59a95f4404a91ef68cc
panchalnehal <nehalpanchal.in@gmail.com>
2026-06-20 11:23:15 +0530

    About component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './index.css';
+
 import HomePage from './landing_page/home/HomePage';
 import Signup from "./landing_page/signup/Signup";
 import AboutPage from "./landing_page/about/AboutPage"
@@ -27,5 +28,5 @@ root.render(
   </Routes>
   <Footer/>
   </BrowserRouter>
-
+  
 );
